585e687d85cbf3d56604e84ac803f89b12cfe220
browser-aguaescura <douglas2000santana@gmail.com>
2024-03-05 22:01:22 -0300

    Conteudo do dia 06-02-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/douglas/sessao2/Text01.java
@@ -0,0 +1,20 @@
+package br.com.senaisp.bauru.douglas.sessao2;
+
+public class Text01 {
+	//	comentarios 
+    public static void main(String[] args) {
+    	//Comentario de uma linha 
+        System.out.println("   /\\         /\\   ");
+        /*
+         * Comentario de duas linhas ou mais
+         *  */
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+    
+}
